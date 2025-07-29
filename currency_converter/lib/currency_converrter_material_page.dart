0a944522fd8ce3dd93ebd14db7e30704afa97064
T-pep <tim@breining.de>
2025-07-29 22:29:29 +0200

    (20.47) I spliit the code on two pages and tryed somthing with the Backgroundcolors. I added a line to the analysis_options.yaml so that the formatter works better (for mor informations open file)

---
+++ currency_converter/lib/currency_converrter_material_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverrterMaterialPage extends StatelessWidget {
+  const CurrencyConverrterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'this is a test',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:currency_converter/currency_converrter_material_page.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: CurrencyConverrterMaterialPage());
+  }
+}
