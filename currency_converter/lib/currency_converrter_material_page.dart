947d95f76651776570df52b25248065c6a1c0e52
T-pep <tim@breining.de>
2025-08-01 10:52:17 +0200

    (11.40) I added an Text an entery field an an Button. The button style is all done with WidgetStateProperty. I change this in the next step. (like in the viedeo)

--- currency_converter/lib/currency_converrter_material_page.dart
+++ currency_converter/lib/currency_converrter_material_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CurrencyConverrterMaterialPage extends StatelessWidget {
@@ -5,18 +7,81 @@ class CurrencyConverrterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 78, 78, 78),
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2.5,
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'this is a test',
+            const Text(
+              '0',
               style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 25,
+                color: Colors.white,
+                fontSize: 50,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Plese enter the amount in USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                  prefixIcon: Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.black,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  debugPrint('hi you stupid idiot clicked the button');
+                },
+                style: ButtonStyle(
+                  elevation: WidgetStatePropertyAll(10),
+                  backgroundColor: WidgetStatePropertyAll(Colors.black),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  padding: WidgetStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  ),
+                  minimumSize: WidgetStatePropertyAll(
+                    Size(double.infinity, 10),
+                  ),
+                ),
+                child: Text('Convert'),
               ),
             ),
           ],
