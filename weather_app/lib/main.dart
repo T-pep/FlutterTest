5ebcd53f8f551d521ffae6d2d5cc1d057701999b
T-pep <tim@breining.de>
2025-08-02 19:08:47 +0200

    (13.24) add new page

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const WeatherScreen(),
+      theme: ThemeData.dark(useMaterial3: true),
+    );
+  }
+}
